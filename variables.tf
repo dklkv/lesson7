f310609792544dc6b750773b18cb2453f7e7683e
Dmitriy Kulikov <kulikov.dmitry@gmail.com>
2024-09-11 09:20:40 +0300

    Add flux parameters

--- variables.tf
+++ variables.tf
@@ -1,16 +1,37 @@
 variable "GOOGLE_REGION" {
-  description = "Регіон для ресурсів Google Cloud"
   type        = string
   default     = "us-central1"
+  description = "GCP project to use"
 }
 
 variable "GOOGLE_PROJECT" {
-  description = "ID проекту Google Cloud"
   type        = string
+  description = "GCP project to use"
 }
 
 variable "GKE_NUM_NODES" {
-  description = "Кількість вузлів у GKE кластері"
   type        = number
   default     = 2
+}
+
+variable "GITHUB_OWNER" {
+  type        = string
+  description = "GitHub owner repository to use"
+}
+
+variable "GITHUB_TOKEN" {
+  type        = string
+  description = "GitHub personal access token"
+}
+
+variable "FLUX_GITHUB_REPO" {
+  type        = string
+  default     = "flux-gitops"
+  description = "Flux GitOps repository"
+}
+
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type        = string
+  default     = "clusters"
+  description = "Flux manifests subdirectory"
 }
